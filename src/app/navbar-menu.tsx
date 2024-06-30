9d18c97c36ceb95b465cb9d4109f0cb9d00e27b9
Ben Weeks <38041074+tinlizard@users.noreply.github.com>
2024-06-30 19:02:57 +0200

    Update navbar-menu.tsx

--- src/app/navbar-menu.tsx
+++ src/app/navbar-menu.tsx
@@ -27,7 +27,7 @@ export default function NavbarMenu({visible}: NavbarInterface) {
                 <Link href="/saved"><li><Image src={"/bookmark.png"} width={30} height={30} alt="bookmark icon" style={{paddingRight:8}}></Image>Saved</li></Link>
                 <Link href="/top-reviewed"><li><Image src={"/star.png"} width={30} height={30} alt="bookmark icon" style={{paddingRight:8}}></Image>Top reviewed bathrooms</li></Link>
                 <Link href="/recents"><li><Image src={"/timer.png"} width={30} height={30} alt="timer icon"></Image>Recents</li></Link>
-                <Link href="/login"><li><Image src={"/login.png"} width={30} height={30} alt="login icon"></Image>Sign in</li></Link>
+                <Link href="/login"><li><Image src={"/login.png"} width={30} height={30} alt="login icon"></Image>Account</li></Link>
                 </ul>
             </div>
         )
