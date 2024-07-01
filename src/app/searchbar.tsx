674bd5b548753b48b52b0c44b770f91b89353b16
Ben Weeks <38041074+tinlizard@users.noreply.github.com>
2024-07-01 22:30:42 +0200

    Update searchbar.tsx

--- src/app/searchbar.tsx
+++ src/app/searchbar.tsx
@@ -1,13 +1,17 @@
 "use client"
 import { useEffect, useState } from "react"
 import Image from "next/image"
-import NavbarMenu from "./navbar-menu"
 import SignInStatus from "./sign-in-status"
+
+interface Toilet {
+  address: string,
+  country: string,
+  city: string,
+}
 
 export default function SearchBar(){
     const [input,setInput] = useState("Enter city/country name, or address...")
-    const [data,setData] = useState("No data")
-    const [visibility,setVisibility] = useState(false)
+    const [data,setData] = useState<Toilet[]>([{address: "No data", country: 'No data', city: 'No data'}]) //default values for data
     const [loggedIn,setLoggedIn] = useState(false)
 
     async function fetchData(){
@@ -43,28 +47,19 @@ export default function SearchBar(){
           </div>
           <div className="searchbar-container">
               <div className="searchbar">
-                <button className="search-menu-button" onClick={()=>setVisibility(!visibility)}>
-                  <Image 
-                  src="/hamburger-menu.png"
-                  width={20}
-                  height={20}
-                  alt="menu button icon"
-                  ></Image>
-                </button>
                 <input type="text" className="searchbar-input" defaultValue={input} onFocus={()=>setInput("")} onChange={(e)=>setInput(e.target.value)}></input>
                 <div>
                  <button className="searchbar-button" onClick={fetchData}>
                   <Image
                     src="/search-icon.png"
-                    width={20}
-                    height={20}
+                    width={50}
+                    height={50}
                     alt="search icon"
                   ></Image>
                  </button>
                 </div>
             </div>
           </div>
-          <NavbarMenu visible={visibility}></NavbarMenu>
         </>
     )
 }
