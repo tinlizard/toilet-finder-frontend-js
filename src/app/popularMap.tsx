de3c72d7258617b3e03197f78848a0e566b37a3e
Ben Weeks <38041074+tinlizard@users.noreply.github.com>
2024-07-03 23:32:50 +0200

    Create popularMap.tsx

---
+++ src/app/popularMap.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { useEffect } from 'react';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+
+export default function PopularMap(){
+    useEffect(() => {
+        const map = new Map({
+            layers: [
+              new TileLayer({
+                source: new OSM(),
+              }),
+            ],
+            target: 'map',
+            view: new View({
+              center: [0, 0],
+              zoom: 2,
+            }),
+          });
+
+          return () => map.setTarget(null);
+    }, [])
+    
+
+    return(
+       <div id="map" style={{width: "400px", height: "300px"}}/>
+    )
+}
