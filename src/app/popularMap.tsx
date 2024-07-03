0cf88ed62a11f285439fbb2fd71849e1db1edaa0
Ben Weeks <38041074+tinlizard@users.noreply.github.com>
2024-07-04 00:37:07 +0200

    Update popularMap.tsx

--- src/app/popularMap.tsx
+++ src/app/popularMap.tsx
@@ -4,6 +4,7 @@ import Map from 'ol/Map.js';
 import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
+import './map.css'
 
 export default function PopularMap(){
     useEffect(() => {
