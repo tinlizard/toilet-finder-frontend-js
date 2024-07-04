78f0a77a1bc6abe7532ab701911fdbe5619a1d87
Ben Weeks <38041074+tinlizard@users.noreply.github.com>
2024-07-04 20:22:46 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,70 @@
 import "./home.css"
 import SearchBar from "./searchbar";
 import PopularMap from "./popularMap";
+import { useState,useEffect,createContext } from "react";
+import { load } from "ol/Image";
+
+export const CoordinateContext = createContext<number[]>([0,0])
 
 export default function Home() {
-  return (
-    <div>
-      <SearchBar></SearchBar>
+  const [latitude, setLatitude] = useState<number>(0)
+  const [longitude, setLongitude] = useState<number>(0)
+  const [coordinates,setCoordinates] = useState<number[]>([0,0])
+  const [loading,setLoading] = useState<boolean>(true)
+
+  const getLocationInfo = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLatitude(position.coords.latitude);
+        console.log('got latitude')
+        setLongitude(position.coords.longitude);
+        console.log('got longitude')
+        setCoordinates([4,5])
+        console.log(coordinates)
+        setLoading(false)
+      })
+    } else {
+      console.log('Geolocation is NOT Available');
+    }
+  };
+
+  useEffect(() => {
+    getLocationInfo()
+  }, []);
+
+  if(loading){
+    return (
+      <div>
+        <SearchBar></SearchBar>
+        <div className="home-h1">
+          <h1>Loading geolocation data... please make sure you have allowed geolocation services.</h1>
+        </div>
+      </div>
+    )
+  } else {
+    return(
+      <div>
+        <SearchBar></SearchBar>
+        <div className="map-container">
+        <PopularMap></PopularMap>
+      </div>
       <div className="home-container">
       <div className="home-h1">
         <h1>Top-reviewed toilets in your area</h1>
       </div>
       <div className="home-main">
-        <PopularMap></PopularMap>
         <ol>
           <li>
             Toilet 1
-            <p>Placeholder text</p>
+            <p>latitude: {latitude}</p>
           </li>
-          <li>Toilet 2</li>
+          <li>Toilet 2
+            <p>longitude: {longitude}</p>
+          </li>
         </ol>
       </div>
-    </div>
-    </div>
-  );
+      </div>
+      </div>
+    )
+  }
 }
